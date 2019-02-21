ad87f45500d80f6fe4be1fad429cf1bd210fd40c
dabobmarley <dabobmarley@hotmail.com>
2019-02-21 15:56:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ function theBeatlesPlay(musicians, instruments){
 }
 
 function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var i = 0
-  var replaceFacts
+  var replaceFacts = [];
   while ( i < facts.length){
-    replaceFacts.push(fact[i] + "!!!")
+    replaceFacts.push(facts[i] + "!!!")
     i++;
   }
-  return replaceFacts
+  return replaceFacts;
 }
